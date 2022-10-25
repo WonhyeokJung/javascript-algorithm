6dbe31c54cc60e6b60f8bc495f6ac4458cc7961d
WonhyeokJung <wonhyeok.contact@gmail.com>
2022-10-25 10:48:22 +0900

    [fix] leetcode no.1662 add testcase

--- src/leetcode/1662. Check If Two String Arrays are Equivalent.ts
+++ src/leetcode/1662. Check If Two String Arrays are Equivalent.ts
@@ -1,3 +1,12 @@
+/**
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+A string is represented by an array if the array elements concatenated in order forms the string.
+ */
+
 function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
   return word1.join('') === word2.join('');
 };
+
+let [word1, word2]: Array<Array<string>> = [["abc", "d", "defg"], ["abcddefg"]];
+
+console.log(arrayStringsAreEqual(word1, word2));
