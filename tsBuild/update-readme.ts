c0efc691a1e069c13ab03f1eae991538a6a9c818
WonhyeokJung <wonhyeok.contact@gmail.com>
2022-11-10 11:11:41 +0900

    [feat] package.json scripts 이용한 push

--- tsBuild/update-readme.ts
+++ tsBuild/update-readme.ts
@@ -9,15 +9,15 @@ fs.readdirSync('./src/leetcode').forEach((file:string) => {
 let text = `![](https://img.shields.io/badge/JavaScript-gray?&logo=JavaScript)![](https://img.shields.io/badge/TypeScript-lightgray?&logo=TypeScript)
 
 ## LeetCode Problems
-\ No. | Solution |
-------------- | ------------------------------- \
+\ No. | Problem | Solution |
+| ----- | ------------- | ------------------------------- \
 \n`
 for (let file of solvedQuestions) {
   if (file.slice(0, 4) === '0000' || file === 'README.md') {
     continue;
   }
   let query:string = file.slice(6, -3).toLowerCase().replaceAll(' ', '-');
-  text += `|[${file.slice(0, -3)}](https://leetcode.com/problems/${query})|[click](./src/leetcode/${encodeURI(file)})|\n`;
+  text += `|${file.slice(0, 4)}|[${file.slice(6, -3)}](https://leetcode.com/problems/${query})|[click](./src/leetcode/${encodeURI(file)})|\n`;
 }
 
 fs.writeFile('./README.md', text, (err) => {
